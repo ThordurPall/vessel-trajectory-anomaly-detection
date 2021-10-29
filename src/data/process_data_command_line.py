d25c9e46f15b5f1a24a99c9af8d0420992c6ba0c
Thordur Pall Fjalarsson <thordurpall@gmail.com>
2021-10-29 22:25:01 +0200

    Add process data properties similar to the ones used in the main paper

--- src/data/process_data_command_line.py
+++ src/data/process_data_command_line.py
@@ -17,20 +17,33 @@ def main():  # main(input_filepath, output_filepath):
 
     process_data = ProcessData(
         # ["cargo", "tanker"],
-        ["fishing"],
-        # ["fishing", "cargo", "tanker"],
-        "All",
-        datetime.datetime.strptime("2019-04-01 00:00:00", "%Y-%m-%d %H:%M:%S"),
-        datetime.datetime.strptime("2020-03-31 23:59:59", "%Y-%m-%d %H:%M:%S"),
+        # ["fishing"],
+        ["fishing", "cargo", "tanker"],
+        "Bornholm",
+        datetime.datetime.strptime("2019-06-01 00:00:00", "%Y-%m-%d %H:%M:%S"),
+        datetime.datetime.strptime("2019-09-30 23:59:59", "%Y-%m-%d %H:%M:%S"),
+        # datetime.datetime.strptime("2019-04-01 00:00:00", "%Y-%m-%d %H:%M:%S"),
+        # datetime.datetime.strptime("2020-03-31 23:59:59", "%Y-%m-%d %H:%M:%S"),
         # datetime.datetime.strptime("2019-06-01 00:00:00", "%Y-%m-%d %H:%M:%S"),
         # datetime.datetime.strptime("2019-06-30 23:59:59", "%Y-%m-%d %H:%M:%S"),
     )
+    # Note that all times are in seconds
+    # Old settings: 600, 60 * 5259487, 60, 1800
+    # "The detection starts if the track is long enough to be meaningful, here greater or equal to 4 hours."
+    # "Discontiguous voyages (voyages that have the maximum interval between two successive AIS messages
+    # longer than a threshold, here 2 hours) were split into contiguous ones. We re-sampled all voyages
+    # to a resolution of 10 minutes (i.e., {t + 1} − {t} = 10 mins) using a linear interpolation.
+    # Very long voyages were split into smaller tracks from 4 to 24 hours each."
+    min_track_length = 240 * 60  # 240 minutes (4 hours)
+    max_track_Length = 24 * 60 * 60  # 24 hours
+    resample_frequency = 10 * 60  # 10 mintues
+    split_track_length = 120 * 60  # 120 minutes (2 hours)
+
     result_file_name = process_data.process_into_trajectories(
-        600, 60 * 5259487, 60, 1800
+        min_track_length, max_track_Length, resample_frequency, split_track_length
     )
     print(result_file_name)
     # result_file_name = "RegionAll_01042019_31032020_Fish_600_2678400_60"
-    # result_file_name = "RegionAll_01042019_31032020_CargTank_600_2678400_60"
     summary_file_name = process_data.process_trajectories_for_summary_analysis(
         result_file_name
     )
