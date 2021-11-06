b8804a4105c00451d7d72be6471a52530601edd4
Thordur Pall Fjalarsson <thordurpall@gmail.com>
2021-11-07 00:46:21 +0100

    Updating notebooks, utils, and SummaryModels so that the notebooks now call SummaryModels

--- src/visualisation/SummaryModels.py
+++ src/visualisation/SummaryModels.py
@@ -134,36 +134,168 @@ class SummaryModels:
         sns.set_theme(style="whitegrid")
         sns.set_context("paper", rc={"lines.linewidth": 3.0})
         style.use("seaborn-colorblind")
+        sns.set_style("ticks")
 
     # Define a function to read in the training/validation curves for a specified setup
 
+    def load_curves_df(self, setup_type, validation_only=False):
+        """Read learning curves for the current setup
 
-def load_curves_df(self, setup_type, validation_only=False):
-    """Read learning curves for the current setup
+        Parameters
+        ----------
+        setup_type : str
+            Model setup used. This will be the value in the 'Setup type' column
 
-    Parameters
-    ----------
-    setup_type : str
-        Model setup used. This will be the value in the 'Setup type' column
+        validation_only : bool (Defaults to False)
+            When True, only the validation curves are returned
+        """
+        curves_file = self.model_name + "_curves.csv"
+        df = pd.read_csv(self.model_dir / curves_file)
+        df_val = df.iloc[:, 3:6].copy()
+        df_val.columns = ["Loss", "KL divergence", "Reconstruction log probabilities"]
+        df_val["Data set type"] = "Validation"
+        df_val["Epoch"] = df_val.index + 1
 
-    validation_only : bool (Defaults to False)
-        When True, only the validation curves are returned
-    """
-    curves_file = self.model_name + "_curves.csv"
-    df = pd.read_csv(self.model_dir / curves_file)
-    df_val = df.iloc[:, 3:6].copy()
-    df_val.columns = ["Loss", "KL divergence", "Reconstruction log probabilities"]
-    df_val["Data set type"] = "Validation"
-    df_val["Epoch"] = df_val.index + 1
+        if not validation_only:
+            df_train = df.iloc[:, 0:3].copy()
+            df_train.columns = [
+                "Loss",
+                "KL divergence",
+                "Reconstruction log probabilities",
+            ]
+            df_train["Data set type"] = "Training"
+            df_train["Epoch"] = df_val.index + 1
+            df = pd.concat([df_train, df_val])
+        else:
+            df = df_val
+        df["Setup type"] = setup_type
+        df.reset_index(drop=True, inplace=True)
+        return df
 
-    if not validation_only:
-        df_train = df.iloc[:, 0:3].copy()
-        df_train.columns = ["Loss", "KL divergence", "Reconstruction log probabilities"]
-        df_train["Data set type"] = "Training"
-        df_train["Epoch"] = df_val.index + 1
-        df = pd.concat([df_train, df_val])
-    else:
-        df = df_val
-    df["Setup type"] = setup_type
-    df.reset_index(drop=True, inplace=True)
-    return df
+    def plot_curves(
+        self,
+        df,
+        hue="Data set type",
+        hue_order=["Training", "Validation"],
+        title=None,
+        file_name=None,
+        xlims=None,
+        ylims=None,
+        fig_size=(18, 5),
+    ):
+        """Plots the loss, KL divergence, and reconstruction log probabilities side by side
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            Data to use when plotting the learning curves
+
+        hue : str (Defaults to None)
+            Variable that determines the color of plot elements
+
+        hue_order : list (Defaults to None)
+            Specify the order of processing and plotting for categorical levels of hue
+
+        title : str
+            Overall figure title
+
+        file_name : str (Defaults to None)
+            File name where the figure will be saved
+
+        xlims : list (Defaults to None)
+            Limit of the x-axis
+
+        ylims : list (Defaults to None)
+            Limit of the y-axis
+
+        fig_size : tuple
+            The overall figure size
+        """
+        _, ax = plt.subplots(1, 3, figsize=fig_size)
+        x = "Epoch"
+        sns.lineplot(x=x, y="Loss", hue=hue, hue_order=hue_order, data=df, ax=ax[0])
+        sns.lineplot(
+            x=x, y="KL divergence", hue=hue, hue_order=hue_order, data=df, ax=ax[1]
+        )
+        sns.lineplot(
+            x=x,
+            y="Reconstruction log probabilities",
+            hue=hue,
+            hue_order=hue_order,
+            data=df,
+            ax=ax[2],
+        )
+        sns.despine()
+
+        if xlims is not None:
+            ax[0].set(xlim=xlims[0])
+            ax[1].set(xlim=xlims[1])
+            ax[2].set(xlim=xlims[2])
+
+        if ylims is not None:
+            ax[0].set(ylim=ylims[0])
+            ax[1].set(ylim=ylims[1])
+            ax[2].set(ylim=ylims[2])
+
+        if title is not None:
+            plt.suptitle(title)
+
+        file_path = None
+        if file_name is not None:
+            file_path = self.learning_curve_dir / (file_name + ".pdf")
+
+        # Check whether to save or plot the figure
+        if self.save_figures & (file_path is not None):
+            plt.savefig(file_path, bbox_inches="tight")
+        if self.plot_figures:
+            plt.show()
+
+    def plot_curves_large(
+        self, df, hue="Data set type", hue_order=["Training", "Validation"], title=None
+    ):
+        """Plot the loss, KL divergence, and reconstruction log probabilities one at a time (large)
+
+        Parameters
+        ----------
+        df : pandas.DataFrame
+            Data to use when plotting the learning curves
+
+        hue : str (Defaults to None)
+            Variable that determines the color of plot elements
+
+        hue_order : list (Defaults to None)
+            Specify the order of processing and plotting for categorical levels of hue
+
+        title : str
+            Overall figure title
+        """
+        # Training and validation loss
+        x = "Epoch"
+        plt.figure()
+        ax = sns.lineplot(x=x, y="Loss", hue=hue, hue_order=hue_order, data=df)
+        sns.despine()
+        if title is not None:
+            ax.set_title(title)
+
+        # Training and validation Kullback–Leibler divergence
+        plt.figure()
+        ax = sns.lineplot(x=x, y="KL divergence", hue=hue, hue_order=hue_order, data=df)
+        sns.despine()
+        if title is not None:
+            ax.set_title(title)
+
+        # Training and validation Reconstruction log probabilities
+        plt.figure()
+        ax = sns.lineplot(
+            x=x,
+            y="Reconstruction log probabilities",
+            hue=hue,
+            hue_order=hue_order,
+            data=df,
+        )
+        sns.despine()
+        if title is not None:
+            ax.set_title(title)
+
+        if self.plot_figures:
+            plt.show()
