e3ffe87609ca68be30f3cdf6ea049431ff3aa449
Thordur Pall Fjalarsson <thordurpall@gmail.com>
2022-01-11 15:43:28 +0100

    Adding to scripts that create report ready figures

--- src/report/continuous_representation_reconstruction.py
+++ src/report/continuous_representation_reconstruction.py
@@ -12,7 +12,8 @@ def main():  # main(input_filepath, output_filepath):
     """Runs code to generate report ready visualization related to
     discrete representation learning curves
     """
-    validation_reconstruction_Bornholm()
+    # validation_reconstruction_Bornholm()
+    validation_reconstruction_Bornholm_with_bias()
     # test_reconstruction_Bornholm()
     # validation_reconstruction_Skagen()
 
@@ -250,6 +251,194 @@ def validation_reconstruction_Bornholm():
     visualise_trajectories.trajectories_fig_dir = tmp
     print(
         {
+            "Reconstructed trajectory": df_recon,
+        }
+    )
+
+
+def validation_reconstruction_Bornholm_with_bias():
+    """Constructs reconstruction plots for models (with bias) in Bornholm"""
+    # Define the region to look into
+    region = "Bornholm"
+    zoom = 8
+
+    # Use the SummaryModels class for everything related to the reconstructions
+    file_name = "RegionBornholm_01062019_30092019_Fish_14400_86400_600"
+    generative_dist = "Diagonal"
+    learning_rate = 0.003
+    scheduler_gamma = [0.6, 0.6, 0.5, 0.6, 0.6, 0.5, 0.5]
+    scheduler_milestones = [25, 50, 100, 150, 200, 250, 400]
+    use_generative_bias = True
+    fig_size = (4, 4)
+    font_scale = 1.5
+
+    # Use the SummaryModels class
+    summary_models = SummaryModels(
+        file_name,
+        generative_dist=generative_dist,
+        learning_rate=learning_rate,
+        scheduler_gamma=scheduler_gamma,
+        scheduler_milestones=scheduler_milestones,
+        use_generative_bias=use_generative_bias,
+        font_scale=font_scale,
+        fig_size=fig_size,
+        save_figures=True,
+        plot_figures=False,
+    )
+
+    # Setup the correct foldure structure
+    summary_models.model_fig_dir = (
+        summary_models.project_dir / "figures" / "report" / "models"
+    )
+    summary_models.learning_curve_dir = summary_models.model_fig_dir / "reconstruction"
+
+    # Use the VisualiseTrajectories class for geographically visualising trajectories
+    fig_size = (10, 10)
+    visualise_trajectories = VisualiseTrajectories(
+        region,
+        save_figures=True,
+        plot_figures=False,
+        fig_size=fig_size,
+        zoom=zoom,
+        font_scale=1.5 * font_scale,
+    )
+    processed_data_dir = visualise_trajectories.processed_data_dir
+
+    # Read the info file to know how to read the data file
+    data_file = processed_data_dir / ("data_" + file_name + ".pkl")
+    data_info_file = processed_data_dir / ("datasetInfo_" + file_name + ".pkl")
+    data_info = utils.read_data_info_file(data_info_file)
+
+    # Load the static Google Map image for the RIO
+    img = visualise_trajectories.read_static_map()
+
+    # Get some trajectories to show an example from
+    data = summary_models.run_evaluation_get_n(n=100, worst=True)
+    df_n = data["TrajectoryLevelData"]
+    train_evaluate = data["TrainEvaluateObject"]
+    idx = 0  # Show the worst reconstruction
+
+    # Setup for the plotting
+    fig, ax = visualise_trajectories.visualise_static_map(img)
+
+    # For plotting a single complete vessel trajectory on the static map
+    index = df_n.iloc[idx]["Index"]
+    data_set = AISDataset(
+        file_name, data_info=data_info, indicies=[index], discrete=False
+    )
+    df_actual = utils.get_tracks_from_dataset(data_set, continuous_representation=True)
+
+    # For plotting the corresponding model reconstruction
+    index = df_n.iloc[idx]["Data set Index"]
+    data_set = train_evaluate.validation_dataloader.dataset
+    reconstruction = train_evaluate.track_reconstructions(data_set, index)
+    df_recon = reconstruction["Reconstruction"][2:]  # Warmup
+
+    # Setup actual and reconstruction data frames
+    df_speed = utils.concat_actual_recon(df_actual, df_recon, "Speed")
+    df_course = utils.concat_actual_recon(df_actual, df_recon, "Course")
+    df_speed["Type"] = df_speed["Type"].str.replace("Actual", "Actual speed")
+    df_course["Type"] = df_course["Type"].str.replace("Actual", "Actual course")
+
+    # Do the actual plotting
+    tmp = visualise_trajectories.trajectories_fig_dir
+    visualise_trajectories.trajectories_fig_dir = summary_models.learning_curve_dir
+    visualise_trajectories.plot_single_track(
+        df_actual,
+        ax,
+        use_cmap=True,
+        df_speed=df_speed,
+        # df_course=df_course,
+        fig=fig,
+        rect=[0.125, 0.65, 0.3, 0.23],
+        # rect2=[0.575, 0.72, 0.12, 0.12],
+        file_name="Bornholm_Diagonal_Fishing_Vessel_Reconstruction_Worst_1_with_bias",
+        font_size=37,
+    )
+    visualise_trajectories.trajectories_fig_dir = tmp
+    plt.clf()
+
+    fig, ax = visualise_trajectories.visualise_static_map(img)
+    visualise_trajectories.trajectories_fig_dir = summary_models.learning_curve_dir
+    visualise_trajectories.plot_single_track(
+        df_recon,
+        ax,
+        use_cmap=True,
+        df_course=df_course,
+        fig=fig,
+        # rect=[0.125, 0.65, 0.3, 0.23],
+        rect2=[0.125, 0.65, 0.3, 0.23],
+        file_name="Bornholm_Diagonal_Fishing_Vessel_Reconstruction_Worst_2_with_bias",
+        font_size=37,
+    )
+    visualise_trajectories.trajectories_fig_dir = tmp
+    print(
+        {
+            "Actual trajectory": df_actual,
+            "Reconstructed trajectory": df_recon,
+        }
+    )
+
+    # Do the same things as above for the second worst reconstruction
+    idx = 1
+
+    # Setup for the plotting
+    fig, ax = visualise_trajectories.visualise_static_map(img)
+
+    # For plotting a single complete vessel trajectory on the static map
+    index = df_n.iloc[idx]["Index"]
+    data_set = AISDataset(
+        file_name, data_info=data_info, indicies=[index], discrete=False
+    )
+    df_actual = utils.get_tracks_from_dataset(data_set, continuous_representation=True)
+
+    # For plotting the corresponding model reconstruction
+    index = df_n.iloc[idx]["Data set Index"]
+    data_set = train_evaluate.validation_dataloader.dataset
+    reconstruction = train_evaluate.track_reconstructions(data_set, index)
+    df_recon = reconstruction["Reconstruction"][2:]  # Warmup
+
+    # Setup actual and reconstruction data frames
+    df_speed = utils.concat_actual_recon(df_actual, df_recon, "Speed")
+    df_course = utils.concat_actual_recon(df_actual, df_recon, "Course")
+    df_speed["Type"] = df_speed["Type"].str.replace("Actual", "Actual speed")
+    df_course["Type"] = df_course["Type"].str.replace("Actual", "Actual course")
+
+    # Do the actual plotting
+    tmp = visualise_trajectories.trajectories_fig_dir
+    visualise_trajectories.trajectories_fig_dir = summary_models.learning_curve_dir
+    visualise_trajectories.plot_single_track(
+        df_actual,
+        ax,
+        use_cmap=True,
+        df_speed=df_speed,
+        # df_course=df_course,
+        fig=fig,
+        rect=[0.125, 0.65, 0.3, 0.23],
+        # rect2=[0.575, 0.72, 0.12, 0.12],
+        file_name="Bornholm_Diagonal_Fishing_Vessel_Reconstruction_Worst_3_with_bias",
+        font_size=37,
+    )
+    visualise_trajectories.trajectories_fig_dir = tmp
+    plt.clf()
+
+    fig, ax = visualise_trajectories.visualise_static_map(img)
+    visualise_trajectories.trajectories_fig_dir = summary_models.learning_curve_dir
+    visualise_trajectories.plot_single_track(
+        df_recon,
+        ax,
+        use_cmap=True,
+        df_course=df_course,
+        fig=fig,
+        # rect=[0.125, 0.65, 0.3, 0.23],
+        rect2=[0.125, 0.65, 0.3, 0.23],
+        file_name="Bornholm_Diagonal_Fishing_Vessel_Reconstruction_Worst_4_with_bias",
+        font_size=37,
+    )
+    visualise_trajectories.trajectories_fig_dir = tmp
+    print(
+        {
+            "Actual trajectory": df_actual,
             "Reconstructed trajectory": df_recon,
         }
     )
--- src/report/distribution_exploration.py
+++ src/report/distribution_exploration.py
@@ -21,7 +21,8 @@ def main():  # main(input_filepath, output_filepath):
     """Runs code to generate report ready visualization to use for
     data exploration in the report
     """
-    explore_Gaussian_Bornholm()
+    # explore_Gaussian_Bornholm()
+    explore_Gaussian_Bornholm_with_bias()
 
 
 def explore_Gaussian_Bornholm():
@@ -77,6 +78,67 @@ def explore_Gaussian_Bornholm():
         "Histogram",
         "MeanCourse",
         file_name="Bornholm_mean_course_distribution",
+        xlabel="Mean course (degrees)",
+        # xlim=[0, 360],
+        dist_x=x,
+        dist_y=stats.norm.pdf(x, mu, sigma),
+        stat="density",
+    )
+
+
+def explore_Gaussian_Bornholm_with_bias():
+    """Look into the Bornholm Gaussian distribution found on some test examples"""
+    # Use the SummaryTrajectories class
+    fig_size = (4, 4)
+    summary_file = "RegionBornholm_01062019_30092019_FishCargTank_1_315569220_0_trajectories_summary.csv"
+    summary_trajectories = SummaryTrajectories(
+        summary_file,
+        save_figures=True,
+        plot_figures=False,
+        fig_size=fig_size,
+        date="DateTimeStart",
+        font_scale=1.5,
+    )
+    summary_trajectories.explore_fig_dir = (
+        summary_trajectories.project_dir / "figures" / "report" / "regions" / "Bornholm"
+    )
+    df = summary_trajectories.df
+    df["Track length (sec)"] = pd.to_datetime(df["DateTimeEnd"]) - pd.to_datetime(
+        df["DateTimeStart"]
+    )
+    df["Track length (sec)"] = df["Track length (sec)"].dt.total_seconds().astype(int)
+    df["Ship type"] = df["ShipType"]
+
+    # Plot the mean speed distribution
+    plt.clf()
+    df_hist = df.reset_index()
+    df_hist = df_hist.loc[df_hist["Ship type"] == "Fishing"]
+    df_hist = df_hist.reset_index()
+    mu = 12.55
+    sigma = 0.58
+    x = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 100)
+    summary_trajectories.hist_bar_plot(
+        df_hist,
+        "Histogram",
+        "MeanSpeed",
+        file_name="Bornholm_mean_speed_distribution_with_bias",
+        xlabel="Mean speed (knots)",
+        xlim=[0, 15],
+        dist_x=x,
+        dist_y=stats.norm.pdf(x, mu, sigma),
+        stat="density",
+    )
+
+    # Plot the mean course histogram for fishing vessels
+    plt.clf()
+    mu = 184.8
+    sigma = 100.7
+    x = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 100)
+    summary_trajectories.hist_bar_plot(
+        df_hist,
+        "Histogram",
+        "MeanCourse",
+        file_name="Bornholm_mean_course_distribution_with_bias",
         xlabel="Mean course (degrees)",
         # xlim=[0, 360],
         dist_x=x,
